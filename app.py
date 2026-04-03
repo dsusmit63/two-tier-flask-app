fd0590fa097f14d75de3909da64121c1fe05faab
dsusmit63 <dsusmit63@gmail.com>
2026-04-03 11:23:24 +0000

    modified code based on suggestions given by flake8

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configure MySQL from environment variables
 app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST", "localhost")
 app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER", "default_user")
-app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD", "default_password")
+app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD", "default_password")\
 app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB", "default_db")
 
 # Initialize MySQL
